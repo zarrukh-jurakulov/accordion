950506b17c6957c4eb6fcc94f95752a95d5c54e4
ZarrukhJurakulov <zarrukhjurakulov474@gmail.com>
2021-04-26 12:39:24 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        id: 1,
+        title: 'Do I have to allow the use of cookes ?',
+        info: 'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art',
+    },
+    {
+        id: 2,
+        title: 'How do I change my My Page password ?',
+        info: 'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mud meh vegan gentrify.',
+    },
+    {
+        id: 3,
+        title: 'Do I have to allow the use of cookes ?',
+        info: 'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan',
+    },
+    {
+        id: 4,
+        title: 'Do I have to allow the use of cookes ?',
+        info: 'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mud meh vegan gentrify.',
+    },
+    {
+        id: 5,
+        title: 'Do I have to allow the use of cookes ?',
+        info: 'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan',
+    },
+]
+
+export default questions
